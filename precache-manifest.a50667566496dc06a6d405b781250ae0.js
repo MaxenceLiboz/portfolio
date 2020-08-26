f3fe4fdc5778bef2d0d6b4ec18cf004696676608
Maxence <maxoulib@gmail.com>
2020-08-26 14:32:25 +0200

    Updates

---
+++ precache-manifest.a50667566496dc06a6d405b781250ae0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8301bdcb411c4e81393d7369952bee0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "21387a7e2ae8f2aabd03",
+    "url": "/portfolio/static/css/main.681cd01c.chunk.css"
+  },
+  {
+    "revision": "6f413b3bf97f5b7754da",
+    "url": "/portfolio/static/js/2.b7a3bec2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.b7a3bec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21387a7e2ae8f2aabd03",
+    "url": "/portfolio/static/js/main.53ff1ce9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5afc248fc18706ccef776ce1fb117750",
+    "url": "/portfolio/static/media/Computer.5afc248f.jpg"
+  },
+  {
+    "revision": "99bf6044ae98ef44c5da16d3d346c75f",
+    "url": "/portfolio/static/media/Graphic.99bf6044.jpg"
+  }
+]);
